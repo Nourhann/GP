2d59275642654b079016c60a51477f4eb238f6ff
norhan mahmod <nourzaki596@gmail.com>
2018-04-15 08:30:45 +0200

    edit2

--- src/model2/FXMLDocumentController.java
+++ src/model2/FXMLDocumentController.java
@@ -5,12 +5,15 @@
  */
 package model2;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
@@ -20,13 +23,20 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    public AnchorPane root ;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+    public void loadInitialization() throws IOException{
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("InitializeWindow.fxml"));
+        //System.out.println("model2.FXMLDocumentController.loadInitialization()");
+        root.getChildren().addAll(pane);
+        
+      //  root 
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
