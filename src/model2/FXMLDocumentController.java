d9dddc6d438078054e89ef43a763443844520127
norhan mahmod <nourzaki596@gmail.com>
2018-04-15 23:24:25 +0200

    edit3

--- src/model2/FXMLDocumentController.java
+++ src/model2/FXMLDocumentController.java
@@ -12,8 +12,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -25,15 +28,29 @@ public class FXMLDocumentController implements Initializable {
     private Label label;
     public AnchorPane root ;
     
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
     public void loadInitialization() throws IOException{
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("InitializeWindow.fxml"));
-        //System.out.println("model2.FXMLDocumentController.loadInitialization()");
-        root.getChildren().addAll(pane);
+       // AnchorPane pane = FXMLLoader.load(getClass().getResource("InitializeWindow.fxml"));
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("InitializeWindow.fxml"));
+      Parent root1 = (Parent) fxmlLoader.load();
+      Stage stage = new Stage();
+      stage.setScene(new Scene(root1)); 
+      stage.show();
+        
+      //  root 
+    }
+     public void loadDesignNewTab() throws IOException{
+       // AnchorPane pane = FXMLLoader.load(getClass().getResource("InitializeWindow.fxml"));
+      FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DesignNewTab.fxml"));
+      Parent root1 = (Parent) fxmlLoader.load();
+      Stage stage = new Stage();
+      stage.setScene(new Scene(root1)); 
+      stage.show();
         
       //  root 
     }
